709cc07062abe8fc180d06085f2e6954fc88461c
shivam <shivamcs0049@gmail.com>
2021-01-04 10:40:21 +0530

    heap sort

---
+++ Sorting/HeapSort.java
@@ -0,0 +1,47 @@
+class HeapSort{
+
+    public static void buildHeap(int arr[],int n){
+
+        for(int i=(n-2)/2;i>=0;i--){
+            heapify(arr,n,i);
+        }
+    }
+
+    public static void heapSort(int arr[]){
+        int n = arr.length;
+        buildHeap(arr,n);
+        for(int i=n-1;i>=0;i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr,i,0);// i is length of reduced array
+        }
+    }
+
+    public static void heapify(int arr[],int n,int i){
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2 * i + 2;
+        if(left <n && arr[left] > arr[largest]){
+            largest = left;
+        }
+        if(right < n && arr[right] > arr[largest]){
+            largest = right;
+        }
+        if(largest !=  i){
+            int swap = arr[i];
+            int arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr,n,largest);
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[] = {12, 11, 13, 5, 6, 7}; 
+        heapSort(arr);
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
--- Test.java
+++ Test.java
@@ -1,29 +1,22 @@
+
 class Test{
 
-    public static void mergeSort(int arr[],int low,int high){
-        if(low<high){
-            int mid = low + (high - low )/2;
-            mergeSort(arr,low,mid);
-            mergeSort(arr,mid+1,high);
-            merge(arr,low,mid,high);
-        }
 
-    }
-
-    public static void merge(int arr[],int low,int mid,int high){
-        int n1 = mid-low+1;
-        int n2 = high - mid;
+    public static void  merge(int arr[],int low,int mid,int high){
+        int n1 = mid-low + 1;
+        int n2 = high -mid;
         int left[] = new int[n1];
         int right[] = new int[n2];
+
         for(int i=0;i<n1;i++){
             left[i] = arr[low+i];
         }
-        for(int j=0;j<n2;j++){
+        for(int j = 0;j<n2;j++){
             right[j] = arr[mid+1+j];
         }
-        int i=0;int j=0;int k=low;
-        while(i<n1 && j<n2){
-            if(left[i] <= right[j]){
+        int i=0;int j=0;int k = low;
+        while(i<n1 && j <n2){
+            if(left[i] < right[j]){
                 arr[k++] = left[i++];
             }else{
                 arr[k++] = right[j++];
@@ -35,17 +28,28 @@ class Test{
         while(j<n2){
             arr[k++] = right[j++];
         }
+    }
 
+    public static void mergeSort(int arr[],int low,int high){
+
+        if(low < high){
+            int mid = low + (high-low)/2;
+            mergeSort(arr,low,mid);;
+            mergeSort(arr,mid+1,high);
+            merge(arr,low,mid,high);
+
+        }
     }
 
     public static void main(String args[]){
-        int arr[] = {43,12,9,48,2,23,66};
-        // int arr2[] = {5,6,6,15};
-        int low = 0;
-        int high = 6;
-        mergeSort(arr,low,high);
-        for(int i=0;i<arr.length;i++){
-            System.out.print(" "+arr[i]);
-        }
+         
+         int arr[] = {43,2,9,43,3,8};
+         int low = 0;
+         int high = 5;
+         mergeSort(arr,low,high);
+         int n = arr.length;
+         for(int i=0;i<n;i++){
+             System.out.print(" "+arr[i]);
+         }
     }
 }

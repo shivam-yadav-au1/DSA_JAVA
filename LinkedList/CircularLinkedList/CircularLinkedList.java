0f2606784197946286196f46e85aa02c105b3f4b
shivam <shivamcs0049@gmail.com>
2021-01-20 12:28:54 +0530

    delete kth node

--- LinkedList/CircularLinkedList/CircularLinkedList.java
+++ LinkedList/CircularLinkedList/CircularLinkedList.java
@@ -19,6 +19,35 @@ class CircularLinkedList{
             System.out.print(""+c.data+" ");
             c = c.next;
         }while(c != head);
+        System.out.println("");
+    }
+
+    public static Node deleteHead(Node head){
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return null;
+        }
+        Node current = head;
+        while(current.next != head){
+            current = current.next;
+        }
+        current.next = head.next;
+        return current.next;
+    }
+    // 1 ==> 2 ==>3 ==> 4 ==> 5
+    // 2 ==> 2 ==>3 ==>4 ==>5
+    public static Node deleteHeadEfficient(Node head){
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return null;
+        }
+        head.data = head.next.data;
+        head.next = head.next.next;
+        return head;
     }
 
     public static Node insertAtBegin(Node head,int data){
@@ -36,6 +65,19 @@ class CircularLinkedList{
         return temp;
     }
 
+    public static Node deleteKthNode(Node head,int k){
+        if(head == null) return head;
+        if(k == 1){
+            return deleteHeadEfficient(head);
+        }
+        Node current = head;
+        for(int i=0;i<k - 1;i++){
+            current = current.next;
+        }
+        current.next = current.next.next;
+        return head;
+    }
+
     public static Node insertAtBeginEfficient(Node head,int data){
         Node temp = new Node(data);
         if(head == null){
@@ -50,6 +92,37 @@ class CircularLinkedList{
         return head;
     }
 
+    public static Node insertAtEnd(Node head,int data){
+        Node temp = new Node(data);
+        if(head == null){
+            temp.next = temp;
+        }else{
+            Node current = head;
+            while(current.next != head){
+                current = current.next;
+            }
+            current.next = temp;
+            temp.next = head;
+        }
+        return head;
+    }
+    // 1 2 3 4 
+    // 1-> 7 ->2
+    public static Node insertAtEndEfficient(Node head,int data){
+        Node temp = new Node(data);
+        if(head == null){
+            temp.next = temp;
+            return temp;
+        }else{
+            temp.next = head.next;
+            head.next = temp;
+            int t = temp.data;
+            temp.data = head.data;
+            head.data = t;
+        }
+        return temp;
+    }
+
     public static void main(String args[]){
         Node head = new Node(10);
         head.next = new Node(20);
@@ -61,6 +134,15 @@ class CircularLinkedList{
         System.out.println("After inserting at begining :");
         Node newhead = insertAtBeginEfficient(head,5);
         traverse(newhead);
+        System.out.println("After inserting at end .");
+        newhead = insertAtEndEfficient(newhead,60);
+        traverse(newhead);
+        newhead = deleteHeadEfficient(newhead);
+        traverse(newhead);
+        System.out.println("After deleting at kth node.");
+        newhead = deleteKthNode(newhead,4);
+        traverse(newhead);
+
 
     }
 }

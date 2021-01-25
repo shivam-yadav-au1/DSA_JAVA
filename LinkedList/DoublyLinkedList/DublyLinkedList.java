0c8340af7fc4d1d6568550366e7fb7ca1058df8f
shivam <shivamcs0049@gmail.com>
2021-01-25 21:44:34 +0530

    circular doubly linked list

--- LinkedList/DoublyLinkedList/DublyLinkedList.java
+++ LinkedList/DoublyLinkedList/DublyLinkedList.java
@@ -85,24 +85,55 @@ class DublyLinkedList{
         current.prev.next = null;
         return head;
     }
+     public static void printlistCircular(Node head){
+        if(head==null)return;
+        Node r=head;
+        do{
+            System.out.print(r.data+" ");
+            r=r.next;
+        }while(r!=head);
+    }
+    public static Node insertAtHeadCircularDoubly(Node head,int data){
+           Node temp = new Node(data);
+           if(head == null || head.next == null){
+               return head;
+           }
+           temp.prev = head.prev;
+           temp.next = head;
+           head.prev.next = temp;
+           head.prev = temp;
+           return temp;
+    }
     
     public static void main(String args[]){
-        Node head = new Node(10);
-        Node temp1 = new Node(20);
-        Node temp2 = new Node(30);
-        head.next = temp1;
-        temp1.prev = head;
-        temp1.next = temp2;
-        temp2.prev = temp1;
-        head = insertBegin(head,5);
-        head = insertAtEnd(head,40);
-        printlist(head);
-        head = deleteHead(head);
-        printlist(head);
-        head = deleteLastNode(head);
-        printlist(head);
+        // Node head = new Node(10);
+        // Node temp1 = new Node(20);
+        // Node temp2 = new Node(30);
+        // head.next = temp1;
+        // temp1.prev = head;
+        // temp1.next = temp2;
+        // temp2.prev = temp1;
+        // head = insertBegin(head,5);
+        // head = insertAtEnd(head,40);
+        // printlist(head);
+        // head = deleteHead(head);
+        // printlist(head);
+        // head = deleteLastNode(head);
+        // printlist(head);
         // head = reverse(head);
         // printlist(head);
+         
+        Node head=new Node(10);
+    	Node temp1=new Node(20);
+    	Node temp2=new Node(30);
+    	head.next=temp1;
+    	temp1.next=temp2;
+    	temp2.next=head;
+    	temp2.prev=temp1;
+    	temp1.prev=head;
+    	head.prev=temp2;
+    	head=insertAtHeadCircularDoubly(head,5);
+    	printlistCircular(head);
         
     }
 }
